855b39b936e807534755478254b4a4f0a8313963
Miguel Céspedes <miguelcespedes@outlook.com>
2019-06-09 13:28:56 -0500

    initialize

---
+++ src/main/java/inka/work/Main.java
@@ -0,0 +1,8 @@
+package inka.work;
+
+public class Main {
+	public static void main(String[] args) {
+		Message message = new Message();
+		System.out.println(message.greeting());
+	}
+}
---
+++ src/main/java/inka/work/Message.java
@@ -0,0 +1,7 @@
+package inka.work;
+
+public class Message {
+	public String greeting() {
+		return "Hello World!";
+	}
+}
